523c79fc62c327d2d38bc4e2823640750d1151d3
lewapro <50249720+lewapro@users.noreply.github.com>
2025-05-30 22:46:11 +0300

    Смена HashMap на TreeMap
    Так как вставка элементов в TreeMap не создаёт несортированный список, то не тратится время на операцию .sort в стриме. Была попытка уменьшить количество действий в getOperator, за счёт использования данных вне стрима скорость улучшилась, была попытка использовать меньше toString за счёт большего количества Integer.parseInt, но это замедлило операцию.
    В итоге разница следующая:
    
    HashMap:
    Максимум  320826800 нс
    Сред	  160169300 нс
    Мин	  155393800 нс
    Не найден 247893600 нс
    
    TreeMap:
    Максимум  55202800 нс
    Сред	  21944400 нс
    Мин	  9611900 нс
    Не найден 200323800 нс
    
    Два toString Один Integer.parseInt (Map<Double, String>)
    Заполнение TreeMap: 14820672100 нс
    findOperator: 12827100-30000000 нс
    		
    Один toString Два Integer.parseInt (Map<String, String>)
    Заполнение TreeMap: 14704032200 нс
    findOperator: 31543200-34543200 нс

--- NumberToOperator/numbertoopeator/src/main/java/com/numbertooperator/Main.java
+++ NumberToOperator/numbertoopeator/src/main/java/com/numbertooperator/Main.java
@@ -1,9 +1,9 @@
 package com.numbertooperator;
 
 import java.io.FileInputStream;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 import org.apache.poi.ss.usermodel.Cell;
@@ -13,7 +13,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Main {
     public static void main(String[] args) {
-        Map<Double, String> range = new HashMap<>();
+        Map<Double, String> range = new TreeMap<>();
         readNumbers("numbertoopeator\\\\src\\\\main\\\\java\\\\com\\\\numbertooperator\\\\DEF-9xx.xlsx", range);
         readNumbers("numbertoopeator\\\\src\\\\main\\\\java\\\\com\\\\numbertooperator\\\\ABC-8xx.xlsx", range);
         readNumbers("numbertoopeator\\\\src\\\\main\\\\java\\\\com\\\\numbertooperator\\\\ABC-4xx.xlsx", range);
@@ -23,13 +23,13 @@ public class Main {
             System.out.println("Введите номер телефона в формате 79005551122. Для выхода ввести 'q'");
             String input = sc.nextLine();
             if(input.toUpperCase().equals("Q")){
-            //  sc.close(); не думаю, что в этом есть смысл.
                 break;
             }
+            String ABCDEF = input.substring(1, 4);
             label:
             if(input.length()==11 && !input.matches(".*\\D.*")){
-                findOperator(range, input);
-                break label;
+                    findOperator(range, input, ABCDEF);
+                    break label;
             }
         }
     } 
@@ -54,37 +54,28 @@ public class Main {
                     limitCell = limitRow.getCell(2);
                     operatorCell = operatorRow.getCell(4);
                     ABCDEFCell = ABCDEFRow.getCell(0);
-                    range.put(Double.parseDouble((int)limitCell.getNumericCellValue() + "." + (int)ABCDEFCell.getNumericCellValue() + "" + 1), operatorCell.getStringCellValue());
+                    if(((int)limitCell.getNumericCellValue() + "").length() == 7){ // В базе есть номера, которые не могут существовать?
+                        range.put(Double.parseDouble((int)limitCell.getNumericCellValue() + "." + (int)ABCDEFCell.getNumericCellValue() + "" + 1), operatorCell.getStringCellValue());
+                    }
                     // Так как ключ должен быть уникальным, к нему добавляется код ABC/DEF в виде данных после запятой. 1 добавляется, так как при .900 число сокращается до .9
                 }      
-            //    workbook.close(); также не влияет на скорость
             }
             } catch (Exception e) {
                 System.out.println(e);
             }
     }
 
-    public static void findOperator(Map<Double, String> range, String input) {
+    public static void findOperator(Map<Double, String> range, String input, String ABCDEF) {
+        
+        int numberShortened = Integer.parseInt(input.substring(4, 11));
         String result = (range
                 .keySet()
                 .stream()
-                .sorted()
-                .filter(x->Integer.parseInt(input.substring(4, 11))<=x && x.toString().substring(x.toString().length()-4, x.toString().length()-1).equals(input.substring(1, 4)))
+                .filter(x->numberShortened<=x && x.toString().substring(x.toString().length()-4, x.toString().length()-1).equals(ABCDEF))
                 .limit(1).map(x->range.get(x)).collect(Collectors.joining()));
-                // Чуть выше через комбинацию одинаковых действий вызволяется код ABC/DEF и сравнивается с input.
         if(!result.isBlank()){
-            System.out.println(result);;
+            System.out.println(result);
         }
         else System.out.println(result + "Данные не найдены!");
     } 
-    // Без использования лишней переменной
-    /*    public static String findOperator(Map<Double, String> range, String input) {
-        return (range
-                .keySet()
-                .stream()
-                .sorted()
-                .filter(x->Integer.parseInt(input.substring(4, 11))<=x && x.toString().substring(x.toString().length()-4, x.toString().length()-1).equals(input.substring(1, 4)))
-                .limit(1).map(x->range.get(x)).collect(Collectors.joining()));
-    } 
-    */
 }
