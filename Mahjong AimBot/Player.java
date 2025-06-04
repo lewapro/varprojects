890f944f995a27bd5a86fe420f0ec8f1ec9d9cbe
lewapro <50249720+lewapro@users.noreply.github.com>
2025-06-04 21:21:37 +0300

    Новая стена, изменение счёта shuntsu
    Теперь стена создаётся через метод. 
    Счёт shuntsu работает через поиск по .contains и нахождение .valueOf. Максимально неэффективно, но пока что лучше так.

--- Mahjong AimBot/Player.java
+++ Mahjong AimBot/Player.java
@@ -39,7 +39,7 @@ public class Player {
     // Пока что есть вариант такой: после получении руки все тайлы разделяются на свои личные ArrayList - man pin sou east south... затем проверяется их длина и если она больше трёх, то идёт
     // последующий поиск правильности комбинации (3 одинаковых (kōtsu) или 3 последовательных(shuntsu)), после того как счётчик групп(mentsu) доходит до 4, идёт поиск дубля. 
     // Пока что есть проблема в том, что я тупой и думаю, что если вдруг в начале он посчитает ментзу не там где надо.
-    private ArrayList<String> manInHand = new ArrayList<String>();      // манзу в руке
+    private ArrayList<Integer> manInHand = new ArrayList<Integer>();      // манзу в руке
     private ArrayList<String> pinInHand = new ArrayList<String>();      // пинзу в руке
     private ArrayList<String> souInHand = new ArrayList<String>();      // соузу в руке
     private ArrayList<String> eastInHand = new ArrayList<String>();     // восток в руке
@@ -51,16 +51,17 @@ public class Player {
     private ArrayList<String> redInHand = new ArrayList<String>();      // красные драконы в руке
     private ArrayList<String> mentsuInHand = new ArrayList<String>();   // ментсу составленные из выше перечисленного
     private ArrayList<String> pairInHand = new ArrayList<String>();     // пары в руке
-    private int pairCounter = 7; // счётчик для повторных пар
+    private int pairCounter = 0;//7 // счётчик для повторных пар
     private int pair = 0;        // пары
     private int mentsu = 0;      // готовые триплеты/последовательности
     private int tileCounter = 0; // универсальный счётчик
+    private int doubleCheck = 1;
     public void checkHand(){
         for(int i = 0; i<pairCounter+1;i++){
             manInHand.clear(); pinInHand.clear(); souInHand.clear(); mentsu = 0; mentsuInHand.clear(); pair = 0; pairInHand.clear();
             for(String tile: hand){
                 if(tile.contains("man")){
-                    manInHand.add(tile);
+                    manInHand.add(Integer.parseInt(tile.substring(tile.length()-1)));
                 } 
                 else if(tile.contains("pin")){
                     pinInHand.add(tile);
@@ -71,31 +72,58 @@ public class Player {
                 }
                 System.out.println("Hand has: \n" + manInHand + " man, \n" + pinInHand + " pin, \n" + souInHand + " sou.");
                 if(manInHand.size()>=2){ // ПОИСК ДУБЛЕЙ!! Нужно поменять на поиск ВСЕХ мастей
-                    for(String tile: manInHand){
+                    if(doubleCheck>=1){
+                        System.out.println("Double check initiated!!!!!!!!!!!!!!!!!");
+                        System.out.println(doubleCheck + "!!!!!!!!");
+                        doubleCheck-=1;
+                    for(Integer tile: manInHand){
                         tileCounter++;
                         System.out.println("looking for a double! " + manInHand.get(tileCounter-1+pairCounter) + " " + manInHand.get(tileCounter+pairCounter));
-                        if(manInHand.get(tileCounter-1+pairCounter).charAt(tile.length()-1)==manInHand.get(tileCounter+pairCounter).charAt(tile.length()-1)){
-                            System.out.println("there is a double! " + (tileCounter-1+pairCounter) + " " + (tileCounter+pairCounter));
-                            pairInHand.add(manInHand.get(tileCounter+pairCounter));
-                            pairInHand.add(manInHand.get(tileCounter-1+pairCounter));
+                        if(manInHand.get(tileCounter-1+pairCounter).equals(manInHand.get(tileCounter+pairCounter))){
+                            System.out.println("there is a double! " + (manInHand.get(tileCounter+pairCounter) + " " + (manInHand.get(tileCounter-1+pairCounter))));
+                            pairInHand.add("a_man" + (manInHand.get(tileCounter+pairCounter)));
+                            pairInHand.add("a_man" + (manInHand.get(tileCounter-1+pairCounter)));
                             manInHand.remove(tileCounter+pairCounter);
                             manInHand.remove(tileCounter-1+pairCounter);
                             pair+=1;
                             pairCounter = tileCounter+1;
+                            doubleCheck-=1;
+                            if(pairInHand.isEmpty()){
+                                doubleCheck+=2;
+                                break;
+                            }
                             break;
                         }
-                        
+                    }
                     }
                 }
-                for(int j = 0; j<manInHand.size();j++){             // нужно изменить на поиск по .contains() и сбор через .indexOf(). Текущая версия не видит дальше +-1 и поэтому есть ошибки.
-                    tileCounter=0;
-                    for(String tile: manInHand){
+                if(pair!=1){
+                    doubleCheck+=1;
+                } else
+                for(int j = 0; j<14;j++){             // нужно изменить на поиск по .contains() и сбор через .indexOf(). Текущая версия не видит дальше +-1 и поэтому есть ошибки.
+                    tileCounter=-1;
+                    for(tileCounter++; tileCounter!=manInHand.size()-2;tileCounter++){
                         if(manInHand.size()>=3){
-                            System.out.println((char)(tile.charAt(tile.length()-1)+1));
-                            System.out.println(manInHand.indexOf(tile));
-                            tileCounter++;
-                            if(tileCounter+1<manInHand.size() // анти проверка на 10й тайл
-                            && tile.charAt(tile.length()-1)==(char)((manInHand.get(tileCounter).charAt(tile.length()-1))-1) // проверка, что следующий тайл на один меньше
+                            int tile = manInHand.get(tileCounter);
+                            if(tileCounter<manInHand.size()){
+                                if(manInHand.contains(tile) && manInHand.contains(tile+1) && manInHand.contains(tile+2)){
+                                    System.out.println("TILE1: " + (tile) + " TILE2: " + (tile+1) + " TILE3: " + (tile+2));
+                                    mentsu++;
+                                    mentsuInHand.add("a_man" + manInHand.get(manInHand.indexOf(tile)));
+                                    mentsuInHand.add("a_man" + manInHand.get(manInHand.indexOf(tile+1)));
+                                    mentsuInHand.add("a_man" + manInHand.get(manInHand.indexOf(tile+2)));
+                                    manInHand.remove(manInHand.indexOf(tile+2));
+                                    manInHand.remove(manInHand.indexOf(tile+1));
+                                    manInHand.remove(manInHand.indexOf(tile));
+                                    System.out.println("MENTSU: " + mentsu + mentsuInHand);
+                                    System.out.println(manInHand);
+                                    tileCounter-=3;
+                                    break;
+                                } 
+
+                            } 
+                                
+                            /*&& tile.charAt(tile.length()-1)==(char)((manInHand.get(tileCounter).charAt(tile.length()-1))-1) // проверка, что следующий тайл на один меньше
                             && tile.charAt(tile.length()-1)==(char)((manInHand.get(tileCounter+1).charAt(tile.length()-1))-2)){ // проверка, что последующий тайл на два меньше
                                 System.out.println("Adding tiles: " + manInHand.get(tileCounter-1) + ", " + manInHand.get(tileCounter) + ", " + manInHand.get(tileCounter+1));
                                 mentsuInHand.add(manInHand.get(tileCounter-1));
@@ -106,15 +134,16 @@ public class Player {
                                 manInHand.remove(tileCounter-1);
                                 mentsu++; // отбираем тайлы в руку ментзу и забираем из основной для следующий вычислений.
                                 System.out.println("non mentsu: \nMAN: " + manInHand.size() + manInHand + "\nPIN: " + pinInHand.size() + pinInHand + "\nSOU: "+ souInHand.size() + souInHand + "\nMENTSU: " + mentsu + mentsuInHand + "\nPAIR: " + pair + pairInHand);
-                                break;    // также здесь можно будет добавить проверки на то, что собрано для вычислений Яку, например инт на шунтсу для пинфу на примере ментсу
-                            } 
+                                break;    // также здесь можно будет добавить проверки на то, что собрано для вычислений Яку, например инт на шунтсу для пинфу на примере ментсу */
+                            //} 
                         }
 
                 }
-                if(mentsu == 4 && pair == 1){
-                    j = 24;
-                    System.out.println("TSUMO READY");
+                System.out.println(mentsu + ":" + pair);
+                if(mentsu != 4 && pair != 1){
+                    System.out.println("tets");
             }
+                
             }
         }
         System.out.println("non mentsu: \nMAN: " + manInHand.size() + manInHand + "\nPIN: " + pinInHand.size() + pinInHand + "\nSOU: "+ souInHand.size() + souInHand + "\nMENTSU: " + mentsu + mentsuInHand + "\nPAIR: " + pair + pairInHand);
--- Mahjong AimBot/Tiles.java
+++ Mahjong AimBot/Tiles.java
@@ -4,14 +4,18 @@ import java.util.Arrays;
 public class Tiles {
     private ArrayList<String> tiles = new ArrayList<>();
         Tiles(){
-            tiles = new ArrayList<>(Arrays.asList(
-                "a_man1","a_man1","a_man1","a_man1","a_man2","a_man2","a_man2","a_man2","a_man3","a_man3","a_man3","a_man3","a_man4","a_man4","a_man4","a_man4","a_man5","a_man5","a_man5","a_man5","a_man6","a_man6","a_man6","a_man6","a_man7","a_man7","a_man7","a_man7","a_man8","a_man8","a_man8","a_man8","a_man9","a_man9","a_man9","a_man9",
-                     "b_pin1","b_pin1","b_pin1","b_pin1","b_pin2","b_pin2","b_pin2","b_pin2","b_pin3","b_pin3","b_pin3","b_pin3","b_pin4","b_pin4","b_pin4","b_pin4","b_pin5","b_pin5","b_pin5","b_pin5","b_pin6","b_pin6","b_pin6","b_pin6","b_pin7","b_pin7","b_pin7","b_pin7","b_pin8","b_pin8","b_pin8","b_pin8","b_pin9","b_pin9","b_pin9","b_pin9",
-                     "c_sou1","c_sou1","c_sou1","c_sou1","c_sou2","c_sou2","c_sou2","c_sou2","c_sou3","c_sou3","c_sou3","c_sou3","c_sou4","c_sou4","c_sou4","c_sou4","c_sou5","c_sou5","c_sou5","c_sou5","c_sou6","c_sou6","c_sou6","c_sou6","c_sou7","c_sou7","c_sou7","c_sou7","c_sou8","c_sou8","c_sou8","c_sou8","c_sou9","c_sou9","c_sou9","c_sou9",
-                     "d_east","d_east","d_east","d_east","e_south","e_south","e_south","e_south","f_west","f_west","f_west","f_west","g_north","g_north","g_north","g_north",
-                     "h_whitedragon","h_whitedragon","h_whitedragon","h_whitedragon","i_greendragon","i_greendragon","i_greendragon","i_greendragon","j_reddragon","j_reddragon","j_reddragon","j_reddragon"
-                ));
+            restartWall();
         }
+    public void restartWall(){
+        tiles.clear();
+        tiles = new ArrayList<>(Arrays.asList(
+            "a_man1","a_man1","a_man1","a_man1","a_man2","a_man2","a_man2","a_man2","a_man3","a_man3","a_man3","a_man3","a_man4","a_man4","a_man4","a_man4","a_man5","a_man5","a_man5","a_man5","a_man6","a_man6","a_man6","a_man6","a_man7","a_man7","a_man7","a_man7","a_man8","a_man8","a_man8","a_man8","a_man9","a_man9","a_man9","a_man9",
+                 "b_pin1","b_pin1","b_pin1","b_pin1","b_pin2","b_pin2","b_pin2","b_pin2","b_pin3","b_pin3","b_pin3","b_pin3","b_pin4","b_pin4","b_pin4","b_pin4","b_pin5","b_pin5","b_pin5","b_pin5","b_pin6","b_pin6","b_pin6","b_pin6","b_pin7","b_pin7","b_pin7","b_pin7","b_pin8","b_pin8","b_pin8","b_pin8","b_pin9","b_pin9","b_pin9","b_pin9",
+                 "c_sou1","c_sou1","c_sou1","c_sou1","c_sou2","c_sou2","c_sou2","c_sou2","c_sou3","c_sou3","c_sou3","c_sou3","c_sou4","c_sou4","c_sou4","c_sou4","c_sou5","c_sou5","c_sou5","c_sou5","c_sou6","c_sou6","c_sou6","c_sou6","c_sou7","c_sou7","c_sou7","c_sou7","c_sou8","c_sou8","c_sou8","c_sou8","c_sou9","c_sou9","c_sou9","c_sou9",
+                 "d_east","d_east","d_east","d_east","e_south","e_south","e_south","e_south","f_west","f_west","f_west","f_west","g_north","g_north","g_north","g_north",
+                 "h_whitedragon","h_whitedragon","h_whitedragon","h_whitedragon","i_greendragon","i_greendragon","i_greendragon","i_greendragon","j_reddragon","j_reddragon","j_reddragon","j_reddragon"
+            ));
+    }
     public ArrayList<String> tiles(){
         return tiles;
     }
